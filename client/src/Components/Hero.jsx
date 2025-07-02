9ec6b7154c6bfc5d80455b337532c5832ca45776
Muneeb khan <139747778+students44@users.noreply.github.com>
2025-07-02 03:14:55 -0700

    searching form compelet

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,10 +1,44 @@
 import React, { useState } from 'react'
 import { assets, cityList } from '../assets/assets'
+import './Hero.css';
+import Swal from 'sweetalert2';
+
+
+
 
 
 const Hero = () => {
+    
 
+  // state for control the select value
   const [pickUpLocation, setPickuPLocation] = useState("");
+
+
+const [pickUpDate, setPickUpDate] = useState('');
+const [returnDate, setReturnDate] = useState('');
+
+
+ const [SearchClick , setSearchClick] = useState("");
+
+ const handleSearchClick = ()=>{
+  if (!pickUpLocation || !pickUpDate || !returnDate) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Missing Information',
+      text: 'Please fill out all fields before searching.',
+    });
+    return;
+  }
+
+  // Example: success alert
+  Swal.fire({
+    icon: 'success',
+    title: 'Searching...',
+    text: `Searching for cars in ${pickUpLocation} from ${pickUpDate} to ${returnDate}.`,
+  });
+ }
+
+
 
   return (
    <div className="h-screen flex flex-col items-center justify-center gap-14 bg-gray-100 text-center">
@@ -15,18 +49,46 @@ const Hero = () => {
       p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0,1)]' >
      
        <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
-        <div>
-          <select name="" id="" required className='py-2'  onChange={(e) => setPickuPLocation(e.target.value)}>
+
+        {/* pick-up your location */}
+        <div className='flex flex-col items-start gap-2 '>
+          <select name="" id="" required className='py-2 pickup-select'  onChange={(e) => setPickuPLocation(e.target.value)}>    
             <option value="">Pick up location</option>
             {cityList.map((e)=> <option value={e}>{e}</option>)}
           </select>
-          <p className='px-1 text-sm text-gray-500'> {pickUpLocation ? pickUpLocation : 'Please select Location'}  </p>
+          <p className='px-1 text-sm text-gray-500 '> {pickUpLocation ? pickUpLocation : 'Please select Location'}  </p>
         </div>
-       </div>
-     
 
+       {/* pick-up your date */}
+         <div className='flex flex-col items-start gap-2'>
+          <label htmlFor="pick-update ">Pick-up Date</label>
+          <input type="date" name='date' id='pick-update' min={new Date().toISOString().split('T')[0]} 
+          className='text-sm text-gray-500'  onChange={(e) => setPickUpDate(e.target.value)} required />
+          {/* <p className='px-1 text-sm text-gray-500 '> {choseDate ? choseDate : 'Please select Location'}  </p> */}
+        </div>
+
+
+        {/* return date */}
+         <div className='flex flex-col items-start gap-2'>
+          <label htmlFor="return-date ">Return Date</label>
+          <input type="date" name='date' id='return-date'
+          className='text-sm text-gray-500' onChange={(e) => setReturnDate(e.target.value)} required/>
+        </div>
+
+        {/* search button */}
+        <button type='button' className='flex items-center justify-center gap-1 px-8 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull
+        text-white rounded-full cursor-pointer'  onClick={handleSearchClick}>
+          
+          <img src={assets.search_icon} alt="Search icon" className='brightness-300' />
+          Search
+          
+          </button>
+
+
+
+       </div>     
       </form>
-
+      {/* hero image */}
       <img src={assets.main_car} alt="hero main car" className='max-h-74'  />
     </div>
   )
