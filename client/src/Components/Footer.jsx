ecb7926a7ca2780e300587c6a83faf270f4cce0a
Muneeb khan <139747778+students44@users.noreply.github.com>
2025-07-04 03:49:56 -0700

    footer contents placed perfectly

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,73 +1,99 @@
-import React from 'react'
-import { assets } from '../assets/assets';
-import {  FaFacebook, FaYoutube, FaGithub , FaLinkedin  } from "react-icons/fa";
-
+import React from "react";
+import { assets } from "../assets/assets";
+import { FaFacebook, FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
-
-
-
-    const linkSections = [
+  const linkSections = [
+    {
+      title: "Quick Links",
+      links: ["Home", "Browse Car", "List Your Car", "About us"],
+    },
+    {
+      title: "Resources",
+      links: [
+        "Help center",
+        "Terms of Service",
+        "Privacy & Policy",
+        "Insurance",
+      ],
+    },
+      {
+      title: "Contact",
+      links: [
+        " 1234 Luxury Car",
+        "Islamabad , G/13/2",
+        "03452645064",
+        "Insurance",
+      ],
+    },
+    {
+      title: "Follow Us",
+      links: [
         {
-            title: "Quick Links",
-            links: ["Home", "Browse Car", "List Your Car", "About us",]
+          name: "Facebook",
+          url: "https://www.facebook.com",
+          icon: <FaFacebook />,
         },
         {
-            title: "Need Help?",
-            links: ["Delivery Information", "Return & Refund Policy", "Payment Methods", "Track your Order", "Contact Us"]
+          name: "YouTube",
+          url: "https://www.youtube.com/",
+          icon: <FaYoutube />,
         },
+        { name: "GitHub", url: "https://www.github.com", icon: <FaGithub /> },
         {
-            title: "Follow Us",
-            links: [    
-    { name: "Facebook", url: "https://www.facebook.com", icon: <FaFacebook /> },
-    { name: "YouTube", url: "https://www.youtube.com/", icon: <FaYoutube /> },
-     { name: "GitHub", url: "https://www.github.com", icon: <FaGithub /> },
-    { name: "LinkedIn", url: "https://www.linkedin.com", icon: <FaLinkedin /> }
-
-  ]
-        }
-    ];
+          name: "LinkedIn",
+          url: "https://www.linkedin.com",
+          icon: <FaLinkedin />,
+        },
+      ],
+    },
+  ];
 
   return (
-   <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-40 pt-5 text-sm text-gray-500 bg-gray-100">
-            <div className="flex flex-col md:flex-row items-start justify-between gap-10 pb-10  border-border-color  text-gray-500">
-                <div>
-                    <img className="w-34 md:w-32" src={assets.logo} alt="footer logo" />
-                    <p className="max-w-[410px] mt-6 text-lg">We Offer All kind`s of rents car for twenty hour for cheap price</p>
-                </div>
-                <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
-                    {linkSections.map((section, index) => (
-                        <div key={index}>
-                            <h3 className="text-base font-medium text-gray-900 uppercase md:mb-5 mb-2">{section.title}</h3>
-                            <ul className="text-md space-y-1">
-  {section.links.map((link, i) => (
-    <li key={i}>
-      {typeof link === "string" ? (
-        <a href="#" className="hover:underline transition">{link}</a>
-      ) : (
-        <a
-          href={link.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:underline transition flex items-center gap-2"
-        >
-          <span className="text-lg">{link.icon}</span>
-          {link.name}
-        </a>
-      )}
-    </li>
-  ))}
-</ul>
-
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright 2025 © PrebuiltUI All Right Reserved.
-            </p>
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-40 pt-5 text-sm text-gray-500 bg-gray-100">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 pb-10  border-border-color  text-gray-500">
+        <div>
+          <img className="w-34 md:w-32" src={assets.logo} alt="footer logo" />
+          <p className="max-w-[410px] mt-6 text-lg">
+            We Offer All kind`s of rents car for twenty hour for cheap price
+          </p>
         </div>
-  )
-}
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {linkSections.map((section, index) => (
+            <div key={index}>
+              <h3 className="text-base font-medium text-gray-900 uppercase md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-md space-y-1">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    {typeof link === "string" ? (
+                      <a href="#" className="hover:underline transition">
+                        {link}
+                      </a>
+                    ) : (
+                      <a
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline transition flex items-center gap-2"
+                      >
+                        <span className="text-lg">{link.icon}</span>
+                        {link.name}
+                      </a>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2025 © PrebuiltUI All Right Reserved.
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
