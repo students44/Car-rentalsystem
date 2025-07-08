d84572c5922db7a9b9618769c20d7a9da629e14b
Muneeb khan <139747778+students44@users.noreply.github.com>
2025-07-08 07:02:06 -0700

    booking car data desgined complete

--- client/src/Pages/MyBookings.jsx
+++ client/src/Pages/MyBookings.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { dummyMyBookingsData } from '../assets/assets';
+import { assets, dummyMyBookingsData } from '../assets/assets';
 import Title from '../Components/Title'; 
 
 const MyBookings = () => {
@@ -21,10 +21,21 @@ const MyBookings = () => {
 
       <div>
         {
-          MyBookings.map((booking , index)=>(
+          booking.map((booking , index)=>(
             <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 p-6
              border border-border-color rounded-lg mt-5 first-mt-12'>
+            {/* car image + car info */}
+            <div className='md:col-spin-1'>
+              <div className='rounded-md overflow-hidden mb-3'>
+                <img src={booking.car.image} alt="booking car image" className='w-full h-auto aspect-video object-cover' />
+              </div>
 
+              <p className='text-lg font-medium mt-3'>{booking.car.brand} {booking.car.modal}</p>
+              
+              <p className='text-gray-500'>{booking.car.year} • {booking.car.category} • 
+                {booking.car.location}
+              </p>
+            </div>
             </div>
           ))
         }
